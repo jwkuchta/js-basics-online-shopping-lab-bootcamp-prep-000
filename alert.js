66b3c4132bf6bccc30ea1303f33987a036851b00
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-14 20:49:31 +0000

    Automatically backed up by Learn

--- alert.js
+++ alert.js
@@ -1,17 +1,4 @@
-//Write a function checkSpam(str) that returns true if str contains ‘viagra’ or ‘XXX’, otherwise false.
-
-//The function must be case-insensitive:
-
 function checkSpam(str) {
-  let lowStr = str.toLowercase();
-
-  return (lowStr.includes("viagra") || upStr.includes("XXX"));
-
-}
-
-alert(checkSpam("VIAGRA"))
-
-/*function checkSpam(str) {
   let lowerStr = str.toLowerCase();
 
   return lowerStr.includes('viagra') || lowerStr.includes('xxx');
