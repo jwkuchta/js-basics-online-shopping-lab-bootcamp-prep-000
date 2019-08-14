1f2c46c85712734de85e0bbbde7b4c8084101729
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-14 21:27:07 +0000

    Automatically backed up by Learn

--- alert.js
+++ alert.js
@@ -1,14 +1,3 @@
-//Create a function truncate(str, maxlength)
-//that checks the length of the str and,
-//if it exceeds maxlength – replaces the end of str
-//with the ellipsis character "…", to make its length equal to maxlength.
+let fruits = ["oranges", "bananas", "grapes"]
 
-function truncate(str, maxlength) {
-  if(str.length < maxlength) {
-    return str
-  } else {
-    return str.slice(0, maxlength - 3) + "..."
-  }
-}
-
-alert(truncate("I wish I were a little bit taller", 10))
+for(fruit in fruits) alert(fruit)
